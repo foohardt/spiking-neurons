5c2d154dcd94f8f1a8f036d78d40eb2484db60d6
Jens Erhardt <erhardt@innoteq-solutions.de>
2022-05-15 11:02:27 +0200

    Added number inputs to network controls.

--- src/NetworkControls.js
+++ src/NetworkControls.js
@@ -21,51 +21,51 @@ const NetworkControls = ({
         <div className="form-group">
           <label>Number of Neurons</label>
           <input
-            type="text"
+            type="number"
             className="form-control"
             id="synapseAvgPerNeuron"
             value={numberOfNeurons}
-            onChange={(e) => setNumberOfNeurons(e.target.value)()}
+            onChange={(e) => setNumberOfNeurons(parseInt(e.target.value))()}
           />
         </div>
         <div className="form-group">
           <label>Avg. Synapes per Neuron</label>
           <input
-            type="text"
+            type="number"
             className="form-control"
             id="synapseAvgPerNeuron"
             value={synapseAvgPerNeuron}
-            onChange={(e) => setSynapseAvgPerNeuron(e.target.value)()}
+            onChange={(e) => setSynapseAvgPerNeuron(parseInt(e.target.value))()}
           />
         </div>
         <div className="form-group">
           <label>Max. Signal Fire Delay</label>
           <input
-            type="text"
+            type="number"
             className="form-control"
             id="signalMaxFireDelay"
             value={signalMaxFireDelay}
-            onChange={(e) => setSignalMaxFireDelay(e.target.value)()}
+            onChange={(e) => setSignalMaxFireDelay(parseInt(e.target.value))()}
           />
         </div>
         <div className="form-group">
           <label>Signal Recovery Delay</label>
           <input
-            type="text"
+            type="number"
             className="form-control"
             id="signalRecoveryDelay"
             value={signalRecoveryDelay}
-            onChange={(e) => setSignalRecoveryDelay(e.target.value)()}
+            onChange={(e) => setSignalRecoveryDelay(parseInt(e.target.value))()}
           />
         </div>
         <div className="form-group">
           <label>Signal Fire Threshold</label>
           <input
-            type="text"
+            type="number"
             className="form-control"
             id="signalFireThreshold"
             value={signalFireThreshold}
-            onChange={(e) => SetSignalFireThreshold(e.target.value)()}
+            onChange={(e) => SetSignalFireThreshold(parseFloat(e.target.value))()}
           />
         </div>
         <span className="align-baseline">
